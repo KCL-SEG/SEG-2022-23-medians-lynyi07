6f0b1d4d49abaffcde4c20b2db4bc515fd3275c2
Lyn Yi <Cheng Lyn Yi>
2022-10-20 13:10:39 +0100

    Done

--- median.py
+++ median.py
@@ -1,6 +1,17 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def get_median(ls): 
+    sorted_list = ls.sort(); 
+    if len(ls) % 2 !=0: 
+        position = int ((len(ls)+1)/2-1)
+        return ls[position]
+    else: 
+        position1 = int(len(ls)/2 - 1)
+        position2 = int(len(ls)/2)
+        return (ls[position1]+ls[position2])/2
+        
+
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
@@ -9,4 +20,4 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+print(get_median(numbers))
